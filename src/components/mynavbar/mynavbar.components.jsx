ad38854e1f961f57faa5b5a28abab7c397bac701
tanzina_akter <tanzinacuet@gmail.com>
2021-02-07 12:06:52 -0600

    navbar commit

---
+++ src/components/mynavbar/mynavbar.components.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+const MyNavbar = () => {
+    return (
+        <>
+            <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark" className="animate-navbar nav-theme justify-content-between">
+  <Navbar.Brand href="#home">Tanzina Akter</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mx-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#projects">Projects</Nav.Link>
+      <Nav.Link href="#testimony">Testimony</Nav.Link>
+      <Nav.Link href="#contact">Contact</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+        </>
+    )
+}
+
+export default MyNavbar
